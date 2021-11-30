e680fe210c69b37b8c196f14e5bab4a1a346b3db
Ryan Sorkin <rsorkin@fred.socs.uoguelph.ca>
2021-11-30 17:13:59 -0500

    moved header code to source code

--- oclgrind.c
+++ oclgrind.c
@@ -1,4 +1,26 @@
-#include "oclgrind.h"
+#ifndef OCLGRIND_H
+#define OCLGRIND_H
+
+#define PROGRAM_FILE "oclgrind.cl"
+#define KERNEL_FUNC "oclgrind"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#ifdef MAC
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+cl_device_id create_device();
+cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename);
+
+#endif
+
 
 int main(int argc, char *argv[])
 {
