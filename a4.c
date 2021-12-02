d71a8c5ff2a7d1bf5c7ecbb7446c012127d617ce
Ryan Sorkin <rsorkin@fred.socs.uoguelph.ca>
2021-12-02 13:49:03 -0500

    pulled master

--- a4.c
+++ a4.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #ifdef MAC
 #include <OpenCL/cl.h>
@@ -19,20 +21,41 @@
 cl_device_id create_device();
 cl_program build_program(cl_context ctx, cl_device_id dev, const char *filename);
 
-char *createGrid(int gridDimensions, int pattern);
-void randomGridInit(char *grid, int dimensions);
-void flipFlopGridInit(char *grid, int gridDimensions);
-void spiderGridInit(char *grid, int gridDimensions);
-void gliderGridInit(char *grid, int gridDimensions);
-void faceGridInit(char *grid, int gridDimensions);
+// Grid initialization methods
+char *createGrid();
+void randomGridInit(char *grid);
+void flipFlopGridInit(char *grid);
+void spiderGridInit(char *grid);
+void gliderGridInit(char *grid);
+void faceGridInit(char *grid);
+
+//display grid method
+void displayGrid(char* grid);
+
+// Argument validation
+void validateArguments(int numArgs, char **args);
+bool isValidArg(char *toCheck);
+
+#define DEFAULT_ERR_MSG "Usage: oclgrind ./a4 <-n #> <-s #> <-i #>"
+#define KERNEL_ERR_MSG "Error: Number of Kernels must be a valid non-negative number greater than or equal to 1."
+#define GRIDSIZE_ERR_MSG "Error: Grid Size must be a valid non-negative number greater than or equal to 7."
+#define INITIALCONFIG_ERR_MSG "Error: Initial Configuration must be a valid non-negative number betwen 0 and 4 inclusive."
 
 #endif
 
+// Global variables initilized with DEFAULT values
+int KERNELS = 1;
+int GRIDSIZE = 20;
+int INITIALCONFIG = 0;
+
 int main(int argc, char *argv[])
 {
-   // TODO: get user value
-   int gridDimensions = 10;
-   int pattern = 1;
+   validateArguments(argc, argv);
+
+	 // print statements below TO BE REMOVED
+	 printf("KERNELS %d\n", KERNELS);
+	 printf("GRIDSIZE %d\n", GRIDSIZE);
+	 printf("INITIALCONFIG %d\n", INITIALCONFIG);
 
    cl_device_id device;
    cl_context context;
@@ -41,10 +64,10 @@ int main(int argc, char *argv[])
    cl_command_queue queue;
    cl_int err, num_groups;
    size_t local_size, global_size;
-   cl_mem inputGridBuffer, outputGridBuffer;
+   cl_mem gridBuffer;
 
-   char *grid = createGrid(gridDimensions, pattern);
-   int gridSize = gridDimensions * gridDimensions + 1;
+   char *grid = createGrid();
+   int gridSize = GRIDSIZE * GRIDSIZE + 1;
 
    /* Create device and context */
    device = create_device();
@@ -54,23 +77,15 @@ int main(int argc, char *argv[])
       perror("Couldn't create a context");
       exit(1);
    }
-
-   /* Build program */
+	 /* Build program */
    program = build_program(context, device, PROGRAM_FILE);
 
-   // TODO: FIGURE THIS OUT
+	 // TODO: FIGURE THIS OUT
    global_size = 8;
    local_size = 4;
    num_groups = global_size / local_size;
 
-   // Create grid buffers
-   inputGridBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, gridSize * sizeof(char), grid, &err);
-   if (err < 0)
-   {
-      perror("Couldn't create a buffer");
-      exit(1);
-   };
-   outputGridBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, gridSize * sizeof(char), grid, &err);
+   gridBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, gridSize * sizeof(char), grid, &err);
    if (err < 0)
    {
       perror("Couldn't create a buffer");
@@ -94,9 +109,8 @@ int main(int argc, char *argv[])
    };
 
    /* Create kernel arguments */
-   err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputGridBuffer);
-   err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &outputGridBuffer);
-   err |= clSetKernelArg(kernel, 2, gridSize * sizeof(char), &gridDimensions);
+   err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &gridBuffer);
+   err |= clSetKernelArg(kernel, 1, gridSize * sizeof(char), &GRIDSIZE);
    if (err < 0)
    {
       perror("Couldn't create a kernel argument");
@@ -104,123 +118,65 @@ int main(int argc, char *argv[])
    }
 
    clReleaseKernel(kernel);
-   clReleaseMemObject(inputGridBuffer);
-   clReleaseMemObject(outputGridBuffer);
+   clReleaseMemObject(gridBuffer);
    clReleaseCommandQueue(queue);
    clReleaseProgram(program);
    clReleaseContext(context);
    return 0;
 }
 
-cl_device_id create_device()
-{
-   cl_platform_id platform;
-   cl_device_id dev;
-   int err;
+cl_device_id create_device() {
+	cl_platform_id platform;
+	cl_device_id dev;
+	int err;
 
-   /* Identify a platform */
-   err = clGetPlatformIDs(1, &platform, NULL);
-   if (err < 0)
-   {
-      perror("Couldn't identify a platform");
-      exit(1);
-   }
+	/* Identify a platform */
+	err = clGetPlatformIDs(1, &platform, NULL);
+	if (err < 0) {
+		perror("Couldn't identify a platform");
+		exit(1);
+	}
 
-   /* Access a device */
-   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
-   if (err == CL_DEVICE_NOT_FOUND)
-   {
-      err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
-   }
-   if (err < 0)
-   {
-      perror("Couldn't access any devicesss");
-      exit(1);
-   }
+	/* Access a device */
+	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
+	if (err == CL_DEVICE_NOT_FOUND) {
+		err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
+	}
+	if (err < 0) {
+		perror("Couldn't access any devicesss");
+		exit(1);
+	}
 
-   return dev;
+	return dev;
 }
 
 /* Create program from a file and compile it */
-cl_program build_program(cl_context ctx, cl_device_id dev, const char *filename)
-{
-   cl_program program;
-   FILE *program_handle;
-   char *program_buffer, *program_log;
-   size_t program_size, log_size;
-   int err;
-
-   /* Read program file and place content into buffer */
-   program_handle = fopen(filename, "r");
-   if (program_handle == NULL)
-   {
-      perror("Couldn't find the program file");
-      exit(1);
-   }
-   fseek(program_handle, 0, SEEK_END);
-   program_size = ftell(program_handle);
-   rewind(program_handle);
-   program_buffer = (char *)malloc(program_size + 1);
-   program_buffer[program_size] = '\0';
-   fread(program_buffer, sizeof(char), program_size, program_handle);
-   fclose(program_handle);
-
-   /* Create program from file */
-   program = clCreateProgramWithSource(ctx, 1,
-                                       (const char **)&program_buffer, &program_size, &err);
-   if (err < 0)
-   {
-      perror("Couldn't create the program");
-      exit(1);
-   }
-   free(program_buffer);
-
-   /* Build program */
-   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-   if (err < 0)
-   {
-
-      /* Find size of log and print to std output */
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
-                            0, NULL, &log_size);
-      program_log = (char *)malloc(log_size + 1);
-      program_log[log_size] = '\0';
-      clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
-                            log_size + 1, program_log, NULL);
-      printf("%s\n", program_log);
-      free(program_log);
-      exit(1);
-   }
-
-   return program;
-}
-
-char *createGrid(int gridDimensions, int pattern)
+char *createGrid()
 {
    // Allocate memory
-   char *grid = (char *)calloc(gridDimensions * gridDimensions + 1, sizeof(char));
-   for (int i = 0; i < gridDimensions * gridDimensions; i++)
+   char *grid = (char *)calloc(GRIDSIZE * GRIDSIZE + 1, sizeof(char));
+   for (int i = 0; i < GRIDSIZE * GRIDSIZE; i++)
    {
       grid[i] = '.';
    }
-   grid[gridDimensions * gridDimensions] = '\0';
+   grid[GRIDSIZE * GRIDSIZE] = '\0';
 
-   switch (pattern)
+   switch (INITIALCONFIG)
    {
    case 0:
-      randomGridInit(grid, gridDimensions);
+      randomGridInit(grid);
       break;
    case 1:
-      flipFlopGridInit(grid, gridDimensions);
+      flipFlopGridInit(grid);
       break;
    case 2:
-      spiderGridInit(grid, gridDimensions);
+      spiderGridInit(grid);
       break;
    case 3:
-      gliderGridInit(grid, gridDimensions);
+      gliderGridInit(grid);
       break;
    case 4:
-      faceGridInit(grid, gridDimensions);
+      faceGridInit(grid);
       break;
 
    default:
@@ -230,13 +186,13 @@ char *createGrid(int gridDimensions, int pattern)
    return grid;
 }
 
-void randomGridInit(char *grid, int dimensions)
+void randomGridInit(char *grid)
 {
    // Initialize random seed
    srand(time(NULL));
 
    // Fills only the first row
-   for (int i = 0; i < dimensions; i++)
+   for (int i = 0; i < GRIDSIZE; i++)
    {
       // 50% chance
       if (rand() / (RAND_MAX / 3) == 0)
@@ -248,19 +204,19 @@ void randomGridInit(char *grid, int dimensions)
 }
 
 // TODO: figure out what happens if grid row length < 4
-void flipFlopGridInit(char *grid, int gridDimensions)
+void flipFlopGridInit(char *grid)
 {
-   int centerIndex = gridDimensions / 2;
+   int centerIndex = GRIDSIZE / 2;
    grid[centerIndex - 2] = 'X';
    grid[centerIndex] = 'X';
    grid[centerIndex + 1] = 'X';
 }
 
 // TODO: figure out what happens if grid row length < 6
-void spiderGridInit(char *grid, int gridDimensions)
+void spiderGridInit(char *grid)
 {
    int spiderLength = 6;
-   int startIndex = (gridDimensions / 2) - 3;
+   int startIndex = (GRIDSIZE / 2) - 3;
    for (int i = startIndex; i < startIndex + spiderLength; i++)
    {
       grid[i] = 'X';
@@ -268,15 +224,141 @@ void spiderGridInit(char *grid, int gridDimensions)
 }
 
 // TODO: figure out what happens if grid row length < 5
-void gliderGridInit(char *grid, int gridDimensions)
+void gliderGridInit(char *grid)
 {
-   flipFlopGridInit(grid, gridDimensions);
-   grid[gridDimensions / 2 + 2] = 'X';
+   flipFlopGridInit(grid);
+   grid[GRIDSIZE / 2 + 2] = 'X';
 }
 
 // TODO: figure out what happens if grid row length < 7
-void faceGridInit(char *grid, int gridDimensions)
+void faceGridInit(char *grid)
 {
-   spiderGridInit(grid, gridDimensions);
-   grid[gridDimensions / 2 + 3] = 'X';
+   spiderGridInit(grid);
+   grid[GRIDSIZE / 2 + 3] = 'X';
+}
+
+cl_program build_program(cl_context ctx, cl_device_id dev, const char *filename) {
+	cl_program program;
+	FILE *program_handle;
+	char *program_buffer, *program_log;
+	size_t program_size, log_size;
+	int err;
+
+	/* Read program file and place content into buffer */
+	program_handle = fopen(filename, "r");
+	if (program_handle == NULL) {
+		perror("Couldn't find the program file");
+		exit(1);
+	}
+	fseek(program_handle, 0, SEEK_END);
+	program_size = ftell(program_handle);
+	rewind(program_handle);
+	program_buffer = (char *)malloc(program_size + 1);
+	program_buffer[program_size] = '\0';
+	fread(program_buffer, sizeof(char), program_size, program_handle);
+	fclose(program_handle);
+
+	/* Create program from file */
+	program = clCreateProgramWithSource(ctx, 1, (const char **)&program_buffer, &program_size, &err);
+	if (err < 0) {
+		perror("Couldn't create the program");
+		exit(1);
+	}
+	free(program_buffer);
+
+	/* Build program */
+	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (err < 0) {
+
+		/* Find size of log and print to std output */
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+		program_log = (char *)malloc(log_size + 1);
+		program_log[log_size] = '\0';
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, log_size + 1, program_log, NULL);
+		printf("%s\n", program_log);
+		free(program_log);
+		exit(1);
+	}
+
+	return program;
+}
+
+
+/**
+ * If any argument is invalid, the function will print the first encountered error and exit the program.
+ * For valid arguments, the GLOBAL VARIABLES will be updated.
+ */
+void validateArguments(int numArgs, char **args) {
+	// number of arguments must be an odd number
+	if (numArgs > 1 && numArgs % 2 == 0) {
+		printf("%s\n", DEFAULT_ERR_MSG);
+		exit(1);
+	}
+
+	// flags are at odd-numbered indicies
+	for (int i = 1; i < numArgs; i = i + 2) {
+		if (strncmp(args[i], "-n", strlen(args[i])) == 0) {
+			if (!isValidArg(args[i+1])) {
+				printf("%s\n", KERNEL_ERR_MSG);
+				exit(1);
+			}
+			else {
+				KERNELS = atoi(args[i+1]);
+				if (KERNELS < 1) {
+					printf("%s\n", KERNEL_ERR_MSG);
+					exit(1);
+				}
+			}
+		}
+		else if (strncmp(args[i], "-s", strlen(args[i])) == 0) {
+			if (!isValidArg(args[i+1])) {
+				printf("%s\n", GRIDSIZE_ERR_MSG);
+				exit(1);
+			}
+			else {
+				GRIDSIZE = atoi(args[i+1]);
+				if (GRIDSIZE < 7) {
+					printf("%s\n", GRIDSIZE_ERR_MSG);
+					exit(1);
+				}
+			}
+		}
+		else if (strncmp(args[i], "-i", strlen(args[i])) == 0) {
+			if (!isValidArg(args[i+1])) {
+				printf("%s\n", INITIALCONFIG_ERR_MSG);
+				exit(1);
+			}
+			else {
+				INITIALCONFIG = atoi(args[i+1]);
+				if (INITIALCONFIG > 4) {
+					printf("%s\n", INITIALCONFIG_ERR_MSG);
+					exit(1);
+				}
+			}
+		}
+		else {
+			printf("%s\n", DEFAULT_ERR_MSG);
+			exit(1);
+		}
+	}
+}
+
+// parameters must be a number greater than 0
+bool isValidArg(char *toCheck) {
+	for (int i = 0; i < strlen(toCheck); i++) {
+		if (!isdigit(toCheck[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void displayGrid(char* grid){
+   for(int i = 0; i<GRIDSIZE*GRIDSIZE; i++){
+      if( i % GRIDSIZE == 0){
+         printf("\n");
+      }
+      printf(" %c ", grid[i]);
+   }
+   printf("\n");
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,14 @@
 Course: CIS*3090
 Assignment: 4
+Group Members:
+
+
+
+Example to run:
+     $ oclgrind ./a4 <-n #> <-s #> <-i #>
+
+
+Parameter Requirements:
+     * Number of Kernels: -n  # : where # must be greater than or equal to 1.
+     * Grid Size: -s  # : where # must be greater than or equal to 7.
+     * Initial Configuration: -i  # : where # must be between 0 and 4 inclusive.
